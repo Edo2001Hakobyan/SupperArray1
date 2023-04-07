015c9852d90e7a0b43865ed4cb0d1ecd468c1bb2
Edo2001Hakobyan <125294848+Edo2001Hakobyan@users.noreply.github.com>
2023-04-07 22:18:15 +0400

    Add files via upload

---
+++ SuperArray.hpp
@@ -0,0 +1,43 @@
+class SuperArray
+{
+	private:
+
+        int m_size;
+        int m_capacity;
+        int  *m_data;
+
+        public:
+               
+	//Consrtuctor definition
+	SuperArray();
+
+	//Function that return array size
+	int getSize();
+
+	//Function that return capacity
+        int getCapacity();
+	
+	//Function that check if array size is equal in capacity,capacity multiply two times 
+	void pushBack(int value);
+
+	//Indexing operator
+	int& operator[](int index);
+        
+	//Overloaded assignment operator
+	SuperArray& operator=(const SuperArray& other);
+
+	//Copy constructor
+	SuperArray(const SuperArray& other); 
+
+	//Function that add value appropriate of index in array
+       	void insert(int index,int value);
+	
+	//Function that remove index in array
+	void remove(int index);
+
+	//Destructor definition
+        ~SuperArray();
+        
+	        
+};
+
